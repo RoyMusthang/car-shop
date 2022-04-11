bbca06231f73ed54d33e5288fb9a22bfbc49bf89
Josué Lobo <darksiders20005@gmail.com>
2022-04-11 18:26:26 -0300

    build: Starting setup

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ class App {
     connectToDatabase();
     this.app.listen(
       PORT,
-      () => console.log(`Server running here 👉 http://localhost:${PORT}`),
+      () => console.log(`Server running http://localhost:${PORT}`),
     );
   }
 
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 // import CustomRouter from './routes/Router';
-// import App from './app';
+import App from './app';
 
 // import exampleController from './controllers/controller-example';
 
 // import { example } from './interfaces/ExampleInterface';
 
-// const server = new App();
+const server = new App();
 
 // const exampleController = new exampleController();
 
@@ -14,4 +14,4 @@
 
 // server.addRouter(exampleRouter.router);
 
-// export default server;
+export default server;
