81224d525e323b387afbd9a3c91cbf35782260c9
Josué Lobo <darksiders20005@gmail.com>
2022-04-11 21:29:50 -0300

    refactor: remove zod od project

--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,11 +1,7 @@
-import { z } from 'zod';
-
-export const schemaVehicle = z.object({
-  model: z.string().min(3),
-  year: z.number().min(1990).max(2022),
-  color: z.string().min(3),
-  status: z.boolean().optional(),
-  buyValue: z.number().int(),
-});
-
-export type Vehicle = z.infer<typeof schemaVehicle>;
+export interface Vehicle {
+  model: string,
+  year: number,
+  color: string,
+  status?: boolean,
+  buyValue: number,
+}
