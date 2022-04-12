1ab7f881051334ddcd86c139aa8b58d12617ce5d
Josué Lobo <darksiders20005@gmail.com>
2022-04-11 22:37:59 -0300

    style: rename mongoModel to MongoAdapter

--- src/model/MongoAdapter.ts
+++ src/model/MongoAdapter.ts
@@ -1,11 +1,11 @@
 import { Model as M, Document } from 'mongoose';
-import Model from '../interfaces/ModelInterface.ts'
+import { Model } from '../interfaces/ModelInterface';
 
 abstract class MongoModel<T> implements Model {
   constructor(protected model: M<T & Document>) { }
 
   public async create(obj: T): Promise<T> {
-    return this.model.create({ ...obj })
+    return this.model.create({ ...obj });
   }
 
   public async read(): Promise<T[]> {
@@ -14,15 +14,15 @@ abstract class MongoModel<T> implements Model {
 
   public async readOne(id: string): Promise<T | null> {
     return this.model.findOne({ _id: id });
-  } 
+  }
 
   public async update(id: string, obj: T): Promise<T | null> {
     return this.model.findOneAndUpdate({ _id: id }, { ...obj });
-  }  
+  }
 
-  public async delete(id: string): Promise<T | null>{
+  public async delete(id: string): Promise<T | null> {
     return this.model.findOneAndDelete({ _id: id });
-  } 
+  }
 }
 
 export default MongoModel;
