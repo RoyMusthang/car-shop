92d6aace18b5b0bbda325874f47109b40ae9dcef
Josué lobo <darksiders20005@gmail.com>
2022-04-25 21:08:26 -0300

    fix: linter

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -5,10 +5,11 @@ abstract class MongoModel<T> implements Model<T> {
   constructor(public model: M<T & Document>) {}
 
   create = async (obj: T): Promise<T> => this.model.create({ ...obj });
+
   read = async (): Promise<T[]> => {
-    const result = await this.model.find()
-    return result
-  }
+    const result = await this.model.find();
+    return result;
+  };
 }
 
 export default MongoModel;
--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -12,7 +12,7 @@ class CustomRouter<T> {
     controller: Controller<T>,
     route: string = controller.route,
   ) {
-    this.router.get(route, controller.read)
+    this.router.get(route, controller.read);
     this.router.post(route, controller.create);
   }
 }
