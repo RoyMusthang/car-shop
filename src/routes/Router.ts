414bec4d99afa0a9ec05aba14a4af2ce1bb4b472
Josué lobo <darksiders20005@gmail.com>
2022-04-25 22:24:40 -0300

    fix: linter

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -14,7 +14,11 @@ class CustomRouter<T> {
     route: string = controller.route,
   ) {
     this.router.get(route, controller.read);
-    this.router.get(`${route}/:id`, middleware.validateIdLength, controller.readOne);
+    this.router.get(
+      `${route}/:id`,
+      middleware.validateIdLength,
+      controller.readOne,
+    );
     this.router.post(route, controller.create);
     this.router.put(`${route}/:id`, controller.update);
     this.router.delete(`${route}/:id`, controller.delete);
