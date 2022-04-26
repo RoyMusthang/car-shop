408c2e81287e8743333941a951ee586e1b2c885d
Josué lobo <darksiders20005@gmail.com>
2022-04-25 22:16:55 -0300

    feat: middleware implemets

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import Controller from '../controllers';
+import middleware from './middlewares/readOne.middleware';
 
 class CustomRouter<T> {
   public router: Router;
@@ -13,7 +14,7 @@ class CustomRouter<T> {
     route: string = controller.route,
   ) {
     this.router.get(route, controller.read);
-    this.router.get(`${route}/:id`, controller.readOne);
+    this.router.get(`${route}/:id`, middleware.validateIdLength, controller.readOne);
     this.router.post(route, controller.create);
     this.router.put(`${route}/:id`, controller.update);
     this.router.delete(`${route}/:id`, controller.delete);
---
+++ src/routes/middlewares/readOne.middleware.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import { RequestWithBody } from "../../controllers";
+import { Car } from "../../interfaces/CarInterface";
+
+const middleware = {
+
+  validateIdLength: (
+    req: RequestWithBody<Car>,
+    res: Response, 
+    next: NextFunction,
+    ) => {
+      const { id } = req.params;
+      if (+(id) < 24) {
+        return res.status(400).json({ message: 'Id must have 24 hexadecimal characters' });
+      }
+      next();
+    }
+}
+
+export default middleware
